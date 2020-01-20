49f9431886a7fee25dcdd58e805bb148ab37fd64
Sagar Chavan <sagar.c.03@gmail.com>
2020-01-20 01:27:27 -0800

    clean

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: [
+    '@babel/polyfill',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  }
+}
