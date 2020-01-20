cf7ae7f9bdc49a047125c265261c67953c94d0f7
Sagar Chavan <sagar.c.03@gmail.com>
2020-01-20 01:32:32 -0800

    add navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faProjectDiagram, faKey } from '@fortawesome/free-solid-svg-icons'
+
+class AppNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+  componentDidMount() {
+    const status = localStorage.getItem('isLoggedIn');
+    if (status !== null && status){
+      this.setState({
+        isLoggedIn: status 
+      })
+    }
+  }
+
+  render() {
+    const logoutButton = <Nav.Link href="/logout/" active={true} style={{color: 'white'}} title="logout">
+      <FontAwesomeIcon icon={faKey} size="2x" />
+    </Nav.Link>;
+
+    return (
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Navbar.Brand href="/" title="TeamWork">
+          <FontAwesomeIcon icon={faProjectDiagram} size="2x" />
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+        {this.state.isLoggedIn ? logoutButton : null}
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default AppNavbar;
